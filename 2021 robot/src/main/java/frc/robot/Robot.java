6a597cb1364405d32cf2e0ad2e26d3125e2bfff3
harrybahena <79483244+harrybahena@users.noreply.github.com>
2021-03-23 17:35:24 -0500

    Added Conveyor Code

--- 2021 robot/src/main/java/frc/robot/Robot.java
+++ 2021 robot/src/main/java/frc/robot/Robot.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 //import edu.wpi.first.wpilibj.Timer;
 
 import edu.wpi.first.wpilibj.PWMSparkMax;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -29,17 +31,25 @@ public class Robot extends TimedRobot {
    * for any initialization code.
    */
 
+// Motor Controllers   
   private CANSparkMax leftMotor = new CANSparkMax(1, MotorType.kBrushless);
   private CANSparkMax rightMotor = new CANSparkMax(2, MotorType.kBrushless);
 
-  private DifferentialDrive drivechain = new DifferentialDrive(leftMotor, rightMotor);
-
-  private Joystick joy1 = new Joystick(0);
-
   private PWMSparkMax leftshooter = new PWMSparkMax(0);
   private PWMSparkMax rightshooter = new PWMSparkMax(1);
+
   private PWMSparkMax arm = new PWMSparkMax(2);
+
   private PWMSparkMax intake = new PWMSparkMax(3);
+
+  private PWMVictorSPX conveyor1 = new PWMVictorSPX(4);
+  private PWMVictorSPX conveyor2 = new PWMVictorSPX(5);
+
+// Drivechain
+  private DifferentialDrive drivechain = new DifferentialDrive(leftMotor, rightMotor);
+
+//Joystick
+  private Joystick joy1 = new Joystick(0);
 
   //private double startTime;
 
@@ -106,14 +116,26 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    boolean shooterspeed0 = joy1.getRawButton(5); // xbox right bumper
+    boolean shooterspeed = joy1.getRawButton(5); // xbox right bumper
     boolean arm_up = joy1.getRawButton(0); // xbox A
     boolean arm_down = joy1.getRawButton(2); //xbox X
+    boolean conveyorup = joy1.getRawButton(4); //UNKNOWN
+    boolean conveyordown = joy1.getRawButton(6); //UNKNOWN
+
+// Runs the Conveyor
+    if (conveyorup) {
+      conveyor1.set(0.55);
+      conveyor2.set(0.5);
+    }
+
+    if (conveyordown) {
+      conveyor1.set(-0.55);
+      conveyor2.set(-0.5);
+    }
 
 //set intake to left and right trigger   
    intake.set(joy1.getRawAxis(3));//intake in
    intake.set(-joy1.getRawAxis(2));// intake out
-   
 
     double flywheel = 0.45; //flywheel speed
     double flywheelstop = 0;
@@ -124,22 +146,27 @@ public class Robot extends TimedRobot {
 
     double left = speed + turn;
     double right = speed - turn;
+
 //drive train control
     drivechain.tankDrive(left, right);
+
 //when intaking balls set the arm to turn toward the ground
     if (joy1.getRawAxis(3) < 0.0){
         arm.set(-.1);
 
     }
+
 //control the arm up and down
     if (arm_up){
-        arm.set (.5);
+        arm.set(.5);
           }
+
     if (arm_down){
       arm.set(-.5);
-//when the shooter is pressed it powers at 0.45
     }
-    if (shooterspeed0) {
+
+//when the shooter is pressed it powers at 0.45, or 45%
+    if (shooterspeed) {
       leftshooter.set(flywheel);
       rightshooter.set(flywheel);
       System.out.println("speed"+flywheel);
@@ -150,6 +177,8 @@ public class Robot extends TimedRobot {
       rightshooter.set(flywheelstop);
       arm.set(0);
       intake.set(0);
+      conveyor1.set(0);
+      conveyor2.set(0);
     }
   }
 
